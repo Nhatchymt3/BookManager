dc0ae2a351682bfc9445a09d8e01952027b703a1
Nhấtchymte <nhatchymte162000@gmail.com>
2021-06-25 16:33:24 +0700

    Initial commit

---
+++ BookManager/Controllers/BookController.cs
@@ -0,0 +1,88 @@
+﻿using BookManager.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BookManager.Controllers
+{
+    public class BookController : Controller
+    {
+        // GET: Book
+        Model1 contex = new Model1();
+        public ActionResult Index()
+        {
+            var listbook = contex.Book.ToList();
+            return View(listbook);
+        }
+        [Authorize]
+        public ActionResult Buy(int id)
+        {
+            Book book = contex.Book.SingleOrDefault(c => c.ID == id);
+            if (book == null)
+            {
+                return HttpNotFound();
+            }
+            return View(book);
+        }
+        [Authorize]
+        public ActionResult Create()
+        {
+
+            return View();
+        }
+        [HttpPost,ActionName("Create")]
+        public ActionResult Create(Book book)
+        {
+            if(ModelState.IsValid)
+            {
+                contex.Book.Add(book);
+                contex.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View();
+            }
+        }
+        [Authorize]
+        public ActionResult Edit(int id)
+        {
+            var db = contex.Book.FirstOrDefault(c => c.ID == id);
+            return View(db);
+        }
+        [HttpPost]
+        public ActionResult Edit(Book book)
+        {
+            Book dbUpdate = contex.Book.FirstOrDefault(p => p.ID == book.ID);
+            if (dbUpdate != null)
+            {
+                contex.Book.AddOrUpdate(book);
+                contex.SaveChanges();
+                return RedirectToAction("Index");
+            }
+                return View();
+            
+        }
+        [Authorize]
+        public ActionResult Delete(int id)
+        {
+            var db = contex.Book.FirstOrDefault(c => c.ID == id);
+            return View(db);
+        }
+        [HttpPost]
+        public ActionResult Delete(Book book)
+        {
+            Book dbDelete = contex.Book.FirstOrDefault(p => p.ID == book.ID);
+            if (dbDelete != null)
+            {
+                contex.Book.Remove(dbDelete);
+                contex.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+    }
+}
---
+++ BookManager/Models/Book.cs
@@ -0,0 +1,28 @@
+﻿namespace BookManager.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("Book")]
+    public partial class Book
+    {
+        
+        public int ID { get; set; }
+        [Required(ErrorMessage = "nhập đi đường có mà lười")]
+        [StringLength(100,ErrorMessage ="nhập ít thôi siêng thế dưới 100")]
+        public string Title { get; set; }
+        [Required(ErrorMessage = "nhập đi đường có mà lười")]
+        public string Description { get; set; }
+        [Required(ErrorMessage = "nhập đi đường có mà lười")]
+        [StringLength(30, ErrorMessage = "nhập ít thôi siêng thế dưới 30")]
+        public string Athour { get; set; }
+        [Required(ErrorMessage = "nhập đi đường có mà lười")]
+        public string Images { get; set; }
+        [Required(ErrorMessage = "nhập đi đường có mà lười")]
+        [Range(1000,1000000,ErrorMessage ="bán vừa thôi 1k đến 1m oke?")]
+        public int? Price { get; set; }
+    }
+}
